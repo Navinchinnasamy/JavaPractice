6ebd543d0eb47a60681f288e122d8b9cae01d8c6
Navin <navinchinnasamy@gmail.com>
2018-05-02 16:27:44 +0530

    Flames with simple GUI
    Get names in Input dialog (a GUI component) and display result in a message dialog

---
+++ FlamesGui.java
@@ -0,0 +1,125 @@
+import java.util.Arrays;
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+
+public class FlamesGui {
+	private static String condition = "FLAMES";
+	
+	public static void main(String args[]){
+		String name1 = JOptionPane.showInputDialog("Please enter name 1: ");
+		String name2 = JOptionPane.showInputDialog("Please enter name 2: ");
+		
+		int remainingCount = getRemainingCount(name1, name2);
+		
+		String[] conditionAry = condition.split("");
+		String result = doFlames(remainingCount, conditionAry);
+		
+		JOptionPane.showMessageDialog(null, "Result : " + finalResultText(result) , "Results", JOptionPane.PLAIN_MESSAGE );
+		System.exit(0);
+	}
+	private static int getRemainingCount(String name1, String name2){
+		String[] name1ary = name1.split("");
+		String[] name2ary = name2.split("");
+		
+		for(int i=0; i<name1ary.length; i++){
+			for(int j=0; j<name2ary.length; j++){
+				if(name1ary[i] != null && name2ary[j] != null && name1ary[i].equals(name2ary[j])){
+					name1ary[i] = null;
+					name2ary[j] = null;
+				}
+			}
+		}
+				
+		for(int i=0; i<name2ary.length; i++){
+			for(int j=0; j<name1ary.length; j++){
+				if(name1ary[j] != null && name2ary[i] != null && name1ary[j].equals(name2ary[i])){
+					name1ary[j] = null;
+					name2ary[i] = null;
+				}
+			}
+		}
+		
+		name1ary = Arrays.stream(name1ary)
+                .filter(value ->
+                        value != null && value.length() > 0
+                )
+                .toArray(size -> new String[size]);
+		name2ary = Arrays.stream(name2ary)
+                .filter(value ->
+                        value != null && value.length() > 0
+                )
+                .toArray(size -> new String[size]);
+				
+		return (name1ary.length + name2ary.length);
+	}
+	
+	private static String doFlames(int remainingCount, String[] conditionAry){
+		String result = "";
+		
+		int conditionLength = conditionAry.length;
+		int deleteIndex = (remainingCount > conditionLength) ? (remainingCount % conditionLength) : remainingCount;
+		deleteIndex = (deleteIndex == 0) ? (conditionLength - 1) : (deleteIndex - 1);
+		
+		conditionAry[deleteIndex] = null;
+		conditionAry = Arrays.stream(conditionAry)
+                .filter(value ->
+                        value != null && value.length() > 0
+                )
+                .toArray(size -> new String[size]);
+		
+		if(conditionAry.length > 1){
+			System.out.println(conditionAry.length+" INDEX "+remainingCount+" ==> "+deleteIndex);
+			System.out.println("B4: "+Arrays.toString(conditionAry));
+			if(deleteIndex < conditionAry.length){
+				conditionAry = flipArray(deleteIndex, conditionAry);
+			}
+			System.out.println("After: "+Arrays.toString(conditionAry));
+			return doFlames(remainingCount, conditionAry);
+		} else {
+			return Arrays.toString(conditionAry);
+		}
+	}
+	
+	private static String[] flipArray(int idx, String[] inputAry){
+		String[] temp = new String[idx];
+		for(int i=0; i+idx<inputAry.length; i++){
+			if(i<idx){
+				temp[idx-1-i] = inputAry[i];
+			}
+			inputAry[i] = inputAry[idx+i];
+		}
+		
+		idx = inputAry.length - 1;
+		for(int j=0; j<temp.length; j++){
+			inputAry[idx-j] = temp[j];
+		}
+		return inputAry;
+	}
+	
+	private static String finalResultText( String result ){
+		switch(result){
+			case "[F]":
+				result = "Friends";
+				break;
+			case "[L]":
+				result = "Lovers";
+				break;
+			case "[A]":
+				result = "Affection";
+				break;
+			case "[M]":
+				result = "Marriage";
+				break;
+			case "[E]":
+				result = "Enemy";
+				break;
+			case "[S]":
+				result = "Sister / Brother";
+				break;
+			default:
+				result = "Something went wrong";
+				break;
+		}
+		return result;
+	}
+}
